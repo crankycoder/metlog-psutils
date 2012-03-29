e178dd4e6f2142e375ce4205c8e2ec27abc4815e
Victor Ng <crankycoder@gmail.com>
2012-03-29 13:34:52 -0400

    added a strong dependency on metlog-py itself

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(name='metlog-psutils',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          # -*- Extra requirements: -*-
+          'metlog-py'
           ],
       extras_require={
           },
